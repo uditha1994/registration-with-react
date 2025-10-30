e4f11f29aebb0519a28584c5190d42f6cdf9763c
Uditha Landekumbura <uditha@programmer.net>
2025-10-30 09:18:56 +0530

    firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: "",
     projectId: "",
     storageBucket: "",
